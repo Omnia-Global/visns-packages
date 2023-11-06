03dcd6da30c1a72844d09c642b63e0d85f1db499
Visns Studio <team@visns.studio>
2023-11-06 15:17:50 +0800

    Update DynamicController to extend base Controller class.

--- src/DynamicController.php
+++ src/DynamicController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Storage;
 
 use Carbon\Carbon;
 
-class DynamicController extends Controller
+class DynamicController extends \App\Http\Controllers\Controller
 {
     protected $model;
 
