e31bc6df8693645ee4849b774ed4a00233740d1d
Visns Studio <team@visns.studio>
2024-05-08 16:06:37 +0800

    add AuditController

---
+++ src/AuditController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Visnsstudio\VisnsPackages;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AuditController extends \App\Http\Controllers\Controller
+{
+    public function show($id)
+    {
+        $data = DB::table("audits")
+            ->with("user")
+            ->where("id", $id)
+            ->first();
+
+        return response()->json($data);
+    }
+
+    public function table(Request $request)
+    {
+        $data = DB::table("audits")->orderBy("created_at", "desc");
+
+        if ($request->has("where") && $request->filled("where")) {
+            foreach ($request->input("where") as $a => $b) {
+                $data->where($b["id"], $b["value"]);
+            }
+        }
+
+        $data = $data->fastPaginate(
+            $request->input("take") ? $request->input("take") : 10
+        );
+
+        return response()->json($data);
+    }
+}
