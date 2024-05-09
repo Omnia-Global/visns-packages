66c5aa338abda8d0307c21f37825dd70070a0837
Visns Studio <team@visns.studio>
2024-05-10 07:33:31 +0800

    Refactor Audit model's user relationship to use fully qualified namespace

--- src/Models/Audit.php
+++ src/Models/Audit.php
@@ -42,6 +42,6 @@ class Audit extends Model
 
     public function user()
     {
-        return $this->belongsTo(App\Models\User::class);
+        return $this->belongsTo(\App\Models\User::class);
     }
 }
