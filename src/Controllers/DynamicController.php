5b55f87cc96bee28b50878afc9aecdcfaa1a7813
Visns Studio <74995890+visnsstudio@users.noreply.github.com>
2024-06-13 19:57:38 +0800

    Refactor DynamicController to include fileable_field in file creation

--- src/Controllers/DynamicController.php
+++ src/Controllers/DynamicController.php
@@ -626,6 +626,7 @@ class DynamicController extends \App\Http\Controllers\Controller
                 "file_name" => $request->input("filename"),
                 "file_extension" => $request->input("extension"),
                 "file_size" => $request->input("filesize"),
+                "fileable_field" => $request->input('key'),
             ]);
 
             // Dynamically attach the file to the resource
